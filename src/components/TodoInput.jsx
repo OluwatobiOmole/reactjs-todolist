0711433478da9a040e09dd377648865a08f7d19a
Tobi Omole <D22125039@mytudublin.ie>
2024-12-01 14:23:46 +0000

    Add Button and Enter message

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,9 @@
 export default function TodoInput () {
     return (
-        <div>
-            TodoInput
-        </div>
+        
+        <header>
+            <input placeholder = "Enter to do ..."/>
+            <button> Add </button>
+        </header>
     )
 }
