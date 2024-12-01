c69da290a28f9a855c96ca0667e15e7a095cfc95
Tobi Omole <D22125039@mytudublin.ie>
2024-12-01 14:59:51 +0000

    Icons added and CSS corrected

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export default function TodoList() {
+
+    let todos = [
+        'Go to the gym',
+        'Eat more fruit',
+        'Pick more food'
+    ]
   return (
-    <div>TodoList</div>
+    <ul className='main'>
+        {todos.map((todo, todoIndex)  => {
+            return (
+                <li className = "todoItem" key = {todoIndex}> {todo}
+                <i class="fa-solid fa-pen-to-square"></i>
+                </li>
+            )
+        })}
+    </ul>
   )
 }
