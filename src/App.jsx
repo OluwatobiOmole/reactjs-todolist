84d86649a4b6827c931e60b64d5dd77c5808df05
Tobi Omole <D22125039@mytudublin.ie>
2024-12-01 15:47:57 +0000

    Updated communication structure between components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import TodoList from "./components/TodoList"
 
 
 function App() {
- 
+  const [todos, setTodos] = useState( [
+    'Go to the gym',
+    'Eat more fruits and Veg',
+    'Pick up kids'
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  
   return (
     <>
       <TodoInput />
