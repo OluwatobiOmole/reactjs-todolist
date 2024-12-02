ad66ac24896958e79fc2e42ddb39f0cfa56ad2aa
Tobi Omole <D22125039@mytudublin.ie>
2024-12-02 03:19:47 +0000

    Create a function for editing items in the todolist

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TodoList from "./components/TodoList"
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
@@ -17,12 +19,18 @@ function App() {
     })
     setTodos (newToDoList)
   }
+  
+  function handleEditTodo (index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
 
+  }
   
   return (
     <>
-      <TodoInput handleAddTodos= {handleAddTodos} />
-      <TodoList handleDeleteTodo = {handleDeleteTodo} todos = {todos} />
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos= {handleAddTodos} />
+      <TodoList handleEditTodo = {handleEditTodo} handleDeleteTodo = {handleDeleteTodo} todos = {todos} />
     </>
   )
 }
